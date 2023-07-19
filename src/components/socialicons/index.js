0108ca4e61dff4f44a982e3167d6f3d7a7029493
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-07-19 12:59:46 -0400

    Modificando contenido del portafolio

--- src/components/socialicons/index.js
+++ src/components/socialicons/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css";
 import {
   FaGithub,
-  FaTwitter,
   FaFacebookF,
   FaLinkedin,
   FaYoutube,
@@ -14,30 +13,23 @@ export const Socialicons = (params) => {
   return (
     <div className="stick_follow_icon">
       <ul>
-        {socialprofils.twitter && (
-          <li>
-            <a href={socialprofils.twitter}>
-              <FaTwitter />
-            </a>
-          </li>
-        )}
         {socialprofils.github && (
           <li>
-            <a href={socialprofils.github}>
+            <a href="https://github.com/robertgrc">
               <FaGithub />
             </a>
           </li>
         )}
         {socialprofils.facebook && (
           <li>
-            <a href={socialprofils.facebook}>
+            <a href="https://www.facebook.com/robertgary.rodriguezcorrales">
               <FaFacebookF />
             </a>
           </li>
         )}
         {socialprofils.linkedin && (
           <li>
-            <a href={socialprofils.linkedin}>
+            <a href="https://www.linkedin.com/in/robert-gary-rodriguez-corrales-a7670122b/">
               <FaLinkedin />
             </a>
           </li>
