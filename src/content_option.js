ef763333650b9b617d1367dea553275d37d4f550
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-09-01 14:47:54 -0400

    cambios en contentoption

--- src/content_option.js
+++ src/content_option.js
@@ -1,220 +1,226 @@
 const logotext = "Robert";
 const meta = {
-    title: "Robert Rodriguez",
-    description: "I’m Robert Rodriguez Full stack devloper,currently working in Bolivia",
+  title: "Robert Rodriguez",
+  description:
+    "I’m Robert Rodriguez Full stack devloper,currently working in Bolivia",
 };
 
 const introdata = {
-    title: "I’m Robert Rodríguez",
-    animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop web apps",
-    },
-    description: "WELCOME TO MY PORTFOLIO AS A DEVELOPER",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+  title: "I’m Robert Rodríguez",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop web apps",
+  },
+  description: "WELCOME TO MY PORTFOLIO AS A DEVELOPER",
+  your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "A bit about my self",
-    aboutme: "Hello! I'm Robert Rodríguez, a passionate programmer with a focus on web development, passionate about technology, I am dedicated to building creative and functional solutions in the frontend area using technologies such as HTML, CSS and JavaScript, and I have experience in the use of frameworks like React My main focus is to create intuitive and attractive user interfaces that provide a seamless experience for users. Besides frontend development, I also have background in backend development using Node.js. I have worked on projects involving API integration, database management and business logic implementation.This portfolio is a reflection of my work and my passion for programming. Here you can explore some of my previous projects and get an idea of my approach to web development. I am excited to continue growing as a professional and face new challenges in the world of software development. If you're looking for someone who is passionate about programming and focused on creating quality digital solutions, I'm excited for the opportunity to collaborate on future projects. Thank you for visiting my portfolio and I look forward to the opportunity to work together!",
+  title: "A bit about my self",
+  aboutme:
+    "Hello! I'm Robert Rodríguez, a passionate programmer with a focus on web development, passionate about technology, I am dedicated to building creative and functional solutions in the frontend area using technologies such as HTML, CSS and JavaScript, and I have experience in the use of frameworks like React My main focus is to create intuitive and attractive user interfaces that provide a seamless experience for users. Besides frontend development, I also have background in backend development using Node.js. I have worked on projects involving API integration, database management and business logic implementation.This portfolio is a reflection of my work and my passion for programming. Here you can explore some of my previous projects and get an idea of my approach to web development. I am excited to continue growing as a professional and face new challenges in the world of software development. If you're looking for someone who is passionate about programming and focused on creating quality digital solutions, I'm excited for the opportunity to collaborate on future projects. Thank you for visiting my portfolio and I look forward to the opportunity to work together!",
 };
-const worktimeline = [{
-        jobtitle: " Full Stack Developer - Mundo Digital ",
-        where: "Cochabamba-Bolivia",
-        date: "2023",
-    },
-    {
-        jobtitle: "web Developer - Mundo Digital",
-        where: "Cochabamba-Bolivia",
-        date: "2022",
-    },
-    {
-        jobtitle: " Front-end Developer - Mundo Digital",
-        where: "Cochabamba-Bolivia",
-        date: "2022",
-    },
+const worktimeline = [
+  {
+    jobtitle: " Full Stack Developer - Mundo Digital ",
+    where: "Cochabamba-Bolivia",
+    date: "2023",
+  },
+  {
+    jobtitle: "web Developer - Mundo Digital",
+    where: "Cochabamba-Bolivia",
+    date: "2022",
+  },
+  {
+    jobtitle: " Front-end Developer - Mundo Digital",
+    where: "Cochabamba-Bolivia",
+    date: "2022",
+  },
 ];
 
-const skills = [{
-        name: "JavaScript",
-        value: 90,
-    },
-    {
-        name: "ReactJS",
-        value: 90,
-    },
-    {
-        name: "GitHub",
-        value: 85,
-    },
-    {
-        name: "NodeJS",
-        value: 70,
-    },
-    {
-        name: "Postman",
-        value: 75,
-    },
-    {
-        name: "HTML",
-        value: 80,
-    },
-    {
-        name: "CSS",
-        value: 85,
-    },
-    {
-        name: "Bootstrap",
-        value: 70,
-    },
-    {
-        name: "Selenium",
-        value: 65,
-    },
-    {
-        name: "JUnit",
-        value: 60,
-    },
+const skills = [
+  {
+    name: "JavaScript",
+    value: 90,
+  },
+  {
+    name: "ReactJS",
+    value: 90,
+  },
+  {
+    name: "GitHub",
+    value: 85,
+  },
+  {
+    name: "NodeJS",
+    value: 70,
+  },
+  {
+    name: "Postman",
+    value: 75,
+  },
+  {
+    name: "HTML",
+    value: 80,
+  },
+  {
+    name: "CSS",
+    value: 85,
+  },
+  {
+    name: "Bootstrap",
+    value: 70,
+  },
+  {
+    name: "Selenium",
+    value: 65,
+  },
+  {
+    name: "JUnit",
+    value: 60,
+  },
 ];
 
 const services = [
-    {
-        title: "Website Development:",
-        description: "Creating responsive and visually appealing websites using HTML, CSS, and JavaScript, with a focus on user experience and usability.",
-    },
-    {
-        title: "API Integration:",
-        description: "API Integration: Connecting and communicating with third-party APIs to incorporate additional functionalities into websites, such as payment gateway integration, social media integration, maps, etc.",
-    },
-    {
-        title: "User Interface Development: ",
-        description: "Designing and developing interactive and dynamic user interfaces using frameworks and libraries like React.",
-    },
-    {
-        title: "Maintenance and Updates:",
-        description: " Providing ongoing support, bug fixes, and updates for existing websites to ensure their long-term functionality and security.",
-    },
-    {
-        title: "Software Quality Testing: ",
-        description: "Provide extensive testing services to ensure software quality, including functional testing, regression testing, performance testing, security testing, usability testing, among others.",
-    },
-    {
-        title: "Test Automation: ",
-        description: "Develop automated test scripts using popular tools and frameworks to streamline the testing process and improve efficiency.",
-    },
+  {
+    title: "Website Development:",
+    description:
+      "Creating responsive and visually appealing websites using HTML, CSS, and JavaScript, with a focus on user experience and usability.",
+  },
+  {
+    title: "API Integration:",
+    description:
+      "API Integration: Connecting and communicating with third-party APIs to incorporate additional functionalities into websites, such as payment gateway integration, social media integration, maps, etc.",
+  },
+  {
+    title: "User Interface Development: ",
+    description:
+      "Designing and developing interactive and dynamic user interfaces using frameworks and libraries like React.",
+  },
+  {
+    title: "Maintenance and Updates:",
+    description:
+      " Providing ongoing support, bug fixes, and updates for existing websites to ensure their long-term functionality and security.",
+  },
+  {
+    title: "Software Quality Testing: ",
+    description:
+      "Provide extensive testing services to ensure software quality, including functional testing, regression testing, performance testing, security testing, usability testing, among others.",
+  },
+  {
+    title: "Test Automation: ",
+    description:
+      "Develop automated test scripts using popular tools and frameworks to streamline the testing process and improve efficiency.",
+  },
 ];
 
 const dataportfolio = [
-    {
-        img: require("./assets/images/hotel-mainpage2.jpg"),
-        description: "Application to manage hotel reservations",
-        link: "https://github.com/robertgrc/ProyectoHotel",
-    },
-    {
-        img: require("./assets/images/tarjeta-reserva.jpg"),
-        description: "Registration Card",
-        link: "https://github.com/robertgrc/ProyectoHotel",
-    },
-    {
-        img: require("./assets/images/comandaRestaurante.jpg"),
-        description: "Restaurant Command",
-        link: "https://github.com/robertgrc/ProyectoHotel",
-    },
-    {
-        img: require("./assets/images/agregarAbonos.jpg"),
-        description: "Add customer payments",
-        link: "https://github.com/robertgrc/ProyectoHotel",
-    },
-    {
-        img: require("./assets/images/controlCuenta.jpg"),
-        description: "Automatic account control of orders, hotel stays and customer payments",
-        link: "https://github.com/robertgrc/ProyectoHotel",
-    },
-    {
-        img: require("./assets/images/tests.jpg"),
-        description: "User Stories and Test Cases for Hotel-App",
-        link: "",
-    },
-    {
-        img: require("./assets/images/sinc-mailchimp.jpg"),
-        description: "Contacts Sync",
-        link: "https://github.com/robertgrc/Mail-chimp",
-    },
-    {
-        img: require("./assets/images/heroes-app-grc.jpg"),
-        description: "Heroes App, shows information of heroes with their respective images .",
-        link: "https://github.com/robertgrc/heroes-app-2022",
-    },
-    {
-        img: require("./assets/images/calendar-app.jpg"),
-        description: "Calendar App, create events in a calendar assigning registered users in backend .",
-        link: "https://github.com/robertgrc/mern-calendar-ferher.git",
-    },
-  
-    // {
-    //     img: "https://picsum.photos/400/700/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-
-    // {
-    //     img: "https://picsum.photos/400/600/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/300/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/550/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
-    // {
-    //     img: "https://picsum.photos/400/700/?grayscale",
-    //     description: "The wisdom of life consists in the elimination of non-essentials.",
-    //     link: "#",
-    // },
+  {
+    img: require("./assets/images/mundodental.jpeg"),
+    description: "Website for MundoDental",
+    link: "https://clinicamundodental.com/",
+  },
+  {
+    img: require("./assets/images/sanrafael.jpeg"),
+    description: "Website for the company san rafael",
+    link: "https://clinicamundodental.com/",
+  },
+  {
+    img: require("./assets/images/countryclub.jpeg"),
+    description: "Website for the country club",
+    link: "https://www.countryclubcba.com/",
+  },
+  {
+    img: require("./assets/images/boliviadent.jpeg"),
+    description: "Website for BoliviaDent",
+    link: "https://clinicaboliviadent.com/",
+  },
+  {
+    img: require("./assets/images/loginscreen.jpeg"),
+    description: "Login Screen HotelApp",
+    link: "https://adorable-axolotl-97199c.netlify.app/",
+  },
+  {
+    img: require("./assets/images/hotelbooking.jpeg"),
+    description: "Application to manage hotel reservations",
+    link: "https://github.com/robertgrc/ProyectoHotel",
+  },
+  {
+    img: require("./assets/images/tarjetareserva.jpeg"),
+    description: "Registration Card",
+    link: "https://github.com/robertgrc/ProyectoHotel",
+  },
+  {
+    img: require("./assets/images/comandarestaurante.jpeg"),
+    description: "Restaurant Command",
+    link: "https://github.com/robertgrc/ProyectoHotel",
+  },
+  {
+    img: require("./assets/images/edicionabonos.jpeg"),
+    description: "Add customer payments",
+    link: "https://github.com/robertgrc/ProyectoHotel",
+  },
+  {
+    img: require("./assets/images/controlcuenta.jpeg"),
+    description:
+      "Automatic account control of orders, hotel stays and customer payments",
+    link: "https://github.com/robertgrc/ProyectoHotel",
+  },
+  {
+    img: require("./assets/images/tests.jpg"),
+    description: "User Stories and Test Cases for Hotel-App",
+    link: "",
+  },
+  {
+    img: require("./assets/images/sinc-mailchimp.jpg"),
+    description: "Contacts Sync",
+    link: "https://github.com/robertgrc/Mail-chimp",
+  },
+  {
+    img: require("./assets/images/valtecniceditarperfil.jpeg"),
+    description: "Edit profile Valtecnic.",
+    link: "https://github.com/robertgrc/heroes-app-2022",
+  },
+  {
+    img: require("./assets/images/valtecnicformulario.jpeg"),
+    description:
+      "Technical visit reservation system for appraisals, the reservation also takes the location of the address on Google Maps",
+    link: "https://github.com/robertgrc/heroes-app-2022",
+  },
+  {
+    img: require("./assets/images/valtecnicmainpage.jpeg"),
+    description: "Main page Valtecnic",
+    link: "https://github.com/robertgrc/heroes-app-2022",
+  },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "robert.rodriguez.rgrc.7@gmail.com",
-    YOUR_FONE: "(+591)70784004",
-    YOUR_LINKEDLN: "linkedin.com/in/robert-gary-rodriguez-corrales-a7670122b",
-    description: "Hi, I'm Robert Rodriguez and I'm a Web Developer in Bolivia and I offer my services in programming and developing all kinds of web projects. ",
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+  YOUR_EMAIL: "robert.rodriguez.rgrc.7@gmail.com",
+  YOUR_FONE: "(+591)70784004",
+  YOUR_LINKEDLN: "linkedin.com/in/robert-gary-rodriguez-corrales-a7670122b",
+  description:
+    "Hi, I'm Robert Rodriguez and I'm a Web Developer in Bolivia and I offer my services in programming and developing all kinds of web projects. ",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  twitter: "https://twitter.com",
 };
 export {
-    meta,
-    dataabout,
-    dataportfolio,
-    worktimeline,
-    skills,
-    services,
-    introdata,
-    contactConfig,
-    socialprofils,
-    logotext,
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
 };
